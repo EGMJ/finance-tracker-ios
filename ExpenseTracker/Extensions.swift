a1f94b1a1affdc7e8d03c248325efea73277e429
egmj <gabrieledson730@gmail.com>
2026-01-11 11:58:08 -0300

    feat: init main screen

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Edson Gabriel Monteiro Jordão on 10/01/26.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let backgroundColor: Color = Color("Background")
+    static let iconColor: Color = Color("Icon")
+    static let textColor: Color = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
