77c11a19974e188291bc7fdb84002f93aef28f33
egmj <gabrieledson730@gmail.com>
2026-01-10 22:33:03 -0300

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Edson Gabriel Monteiro Jordão on 10/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
